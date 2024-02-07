f98c7507cbdac808ff855f67a4355039ee55cfbd
moustafa elsayed <moelsayed949@gmail.com>
2024-02-07 14:52:30 +0200

    styling heroSection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,25 +1,44 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { TypeAnimation } from 'react-type-animation';
+
 
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            Hello, iam moustafa elsayed
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
+        <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-200 to-blue-600">
+              Hello, I am{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Moustafa El-Sayed",
+                1000,
+                "Web Developer",
+                1000,
+                "Mobile Developer",
+                1000,
+                
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
-          <p className="text-white text-lg lg:text-xl">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam,
-            dolorem ipsum nam dolores doloribus unde commodi deleniti
-            voluptates, molestiae debitis sunt beatae culpa impedit rem magni
-            iure animi eaque consequuntur?
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            voluptuous.
           </p>
           <div>
             <Link
               href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-r from-blue-200 to-blue-600 bg-white hover:bg-slate-200 text-black"
             >
               Hire Me
             </Link>
@@ -27,7 +46,7 @@ const HeroSection = () => {
               href="/"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2 border border-white">
                 Download CV
               </span>
             </Link>
