b9f1e17394aeaa78685e97724e7ea8885bcdfccb
moustafa elsayed <moelsayed949@gmail.com>
2024-02-07 18:07:22 +0200

    fixed issue

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,7 +14,7 @@ const NavBar = () => {
             <li>
               <Link
                 href={"/about"}
-                className="text-xl text-Whit hover:text-blue-500 font-bold "
+                className="text-xl text-White  font-bold "
               >
                 Home
               </Link>
@@ -22,7 +22,7 @@ const NavBar = () => {
             <li>
               <Link
                 href={"/about"}
-                className="text-xl text-Whit hover:text-blue-500 font-bold "
+                className="text-xl text-White  font-bold "
               >
                 Projects
               </Link>
@@ -30,7 +30,7 @@ const NavBar = () => {
             <li>
               <Link
                 href={"/about"}
-                className="text-xl text-Whit hover:text-blue-500 font-bold"
+                className="text-xl text-White  font-bold"
               >
                 About me
               </Link>
