6c979d6bcfeb42a245161555a82345547ca0275f
moustafa elsayed <moelsayed949@gmail.com>
2024-02-07 17:40:02 +0200

    fixed issue

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
         <div className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-200 to-blue-600">
-              Hello, I Am
+              Hello, I'm
             </span>
             <br></br>
             <TypeAnimation
