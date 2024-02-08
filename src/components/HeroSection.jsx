48b27e3566e98ea6d6b636dad2312a9e7e47dc47
moustafa elsayed <moelsayed949@gmail.com>
2024-02-08 21:28:58 +0200

    add animation

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,12 +4,18 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
 
 const HeroSection = () => {
   return (
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-12">
-        <div className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-200 to-blue-600">
               Hello, I&apos;m{" "}
@@ -31,8 +37,8 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            I’m Front End Developer Working With ReactJS,NextJs,React Native and constantly
-            learning to expand my knowledge and skills
+            I’m Front End Developer Working With ReactJS,NextJs,React Native and
+            constantly learning to expand my knowledge and skills
           </p>
           <div>
             <Link
@@ -50,7 +56,7 @@ const HeroSection = () => {
               </span>
             </Link>
           </div>
-        </div>
+        </motion.div>{" "}
         <div className="col-span-4 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
