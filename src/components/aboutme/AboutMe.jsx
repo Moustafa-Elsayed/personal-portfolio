121c5602c7116ba44cda192e81ba6021be3a486d
moustafa elsayed <moelsayed949@gmail.com>
2024-02-08 02:22:27 +0200

    add skills

--- src/components/aboutme/AboutMe.jsx
+++ src/components/aboutme/AboutMe.jsx
@@ -3,7 +3,42 @@
 import Image from "next/image";
 import React, { useState, useTransition } from "react";
 import TabButton from "./TabButton";
-
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>PostgreSQL</li>
+        <li>Sequelize</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Fullstack Academy of Code</li>
+        <li>University of California, Santa Cruz</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>AWS Cloud Practitioner</li>
+        <li>Google Professional Cloud Developer</li>
+      </ul>
+    ),
+  },
+];
 const AboutMe = () => {
   const [tab, setTab] = useState("skills");
   const [isPending, startTransition] = useTransition();
@@ -33,7 +68,7 @@ const AboutMe = () => {
             looking to expand my knowledge and skill set. I am a team player and
             I am excited to work with others to create amazing applications.
           </p>
-          <div className="flex flex-row gap-3 mt-10">
+          <div className="flex flex-row justify-start mt-8">
             <TabButton
               selectTab={() => handleTabChange("skills")}
               active={tab === "skills"}
@@ -52,6 +87,9 @@ const AboutMe = () => {
             >
               Certifications
             </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
           </div>
         </div>
       </div>
--- src/components/aboutme/TabButton.jsx
+++ src/components/aboutme/TabButton.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 const TabButton = ({ active, selectTab, children }) => {
-  const buttonClasses = active ? "text-white" : "text-[#ADB7BE]";
+  const buttonClasses = active
+    ? "text-blue-500 border-b border-blue-500"
+    : "text-[#ADB7BE]";
 
   return (
     <button onClick={selectTab}>
-       <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
+      <p
+        className={`mr-3 font-semibold text-2xl hover:text-white ${buttonClasses}`}
+      >
         {children}
       </p>
     </button>
