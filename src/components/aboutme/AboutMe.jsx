a52b3d243d2852fb4f22be366b387eacadfb4926
moustafa elsayed <moelsayed949@gmail.com>
2024-02-08 02:29:21 +0200

    styling

--- src/components/aboutme/AboutMe.jsx
+++ src/components/aboutme/AboutMe.jsx
@@ -61,12 +61,14 @@ const AboutMe = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-2xl lg:text-xl ">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+            I am a <span className="text-blue-500">Frontend web</span> and{" "}
+            <span className="text-blue-500">Mobile App</span> developer with a
+            passion for creating interactive and responsive web applications. I
+            have experience working with JavaScript, ReactJs,NextJs,React
+            Native, Redux, HTML, CSS, and Git. I am a quick learner and I am
+            always looking to expand my knowledge and skill set. I am a team
+            player and I am excited to work with others to create amazing
+            applications.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
