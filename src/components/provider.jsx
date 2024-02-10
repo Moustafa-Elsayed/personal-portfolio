86d75c773efa373dcc66b42c128a63414bfc90be
moustafa elsayed <moelsayed949@gmail.com>
2024-02-10 12:25:01 +0200

    fixed build issue

--- src/components/provider.jsx
+++ src/components/provider.jsx
@@ -1,29 +1,24 @@
+"use client";
 
-"use client"
+import { ThemeProvider } from "next-themes";
+import { useState, useEffect } from "react";
 
-import {ThemeProvider} from 'next-themes';
-import { useState, useEffect } from 'react'
+const Provider = ({ children }) => {
+  const [mounted, setMounted] = useState(false);
 
-
-
-const Provider = ({children}) => {
-
-const [mounted,setMounted] = useState(false);
-
-useEffect (() => {
+  useEffect(() => {
     setMounted(true);
-},[]);
+  }, []);
 
-if(!mounted){
+  if (!mounted) {
     return <>{children}</>;
-}
-
+  }
 
   return (
-    <ThemeProvider enableSystem={true} attribute='class'>
+    <ThemeProvider enableSystem={true} attribute="class">
       {children}
     </ThemeProvider>
-  )
-}
+  );
+};
 
 export default Provider;
