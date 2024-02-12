e90358077b6e433e318b2d07a11fa4b63270ae00
moustafa elsayed <moelsayed949@gmail.com>
2024-02-12 18:46:12 +0200

    edit lightmood skills section

--- src/components/AchievementsSection.jsx
+++ src/components/AchievementsSection.jsx
@@ -16,7 +16,8 @@ const skills = [
     title: "javascript",
   },
   {
-    src: "https://svgshare.com/i/12un.svg",
+    src: "https://svgshare.com/i/130J.svg",
+    title:"React"
   },
   {
     src: "https://svgshare.com/i/12vW.svg",
@@ -35,11 +36,24 @@ const skills = [
     src: "https://svgshare.com/i/12ve.svg",
     title: "Tailwind",
   },
-
   {
     src: "https://svgshare.com/i/12vp.svg",
     title: "Git ",
   },
+  {
+    src: "https://svgshare.com/i/130S.svg",
+    title: "GitHub",
+    
+  },
+  {
+    src: "https://svgshare.com/i/12zg.svg",
+    title: "Material ui",
+  },
+  {
+    src: "https://svgshare.com/i/12zC.svg",
+    title: "Bootstrap",
+  },
+
 ];
 
 const AchievementsSection = () => {
@@ -48,12 +62,12 @@ const AchievementsSection = () => {
 
       <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <div className="text-center pb-6 ">
-          <span className=" text-white p-3 rounded-lg border border-gray-500 font-bold">Skills</span>
+          <span className=" text-black dark:text-white p-3 rounded-lg border border-gray-500 font-bold">Skills</span>
         </div>
-        <div className="text-center pb-6 text-[#ADB7BE]">
+        <div className="text-center pb-6 text-black dark:text-[#ADB7BE]">
           The skills, tools and technologies I am really good at:
         </div>
-        <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between gap-2">
+        <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col flex-wrap sm:flex-row items-center justify-between gap-4">
           {/* {achievementsList.map((achievement, index) => {
             return (
               <div
@@ -85,10 +99,10 @@ const AchievementsSection = () => {
             return (
               <div
                 key={item.id}
-                className="flex flex-col gap-2 justify-center items-center md:self-end "
+                className="flex flex-col gap-2 justify-evenly items-center md:self-end "
               >
                 <Image alt={item.title} src={item.src} width={70} height={50} />
-                <div className="text-gray-200">{item.title}</div>
+                <div className="text-blaack dark:text-gray-200">{item.title}</div>
               </div>
             );
           })}
