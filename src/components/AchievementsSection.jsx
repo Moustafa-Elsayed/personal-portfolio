e4d9dc8415d4a65a31f1a05cc9bf9863b26b11fd
moustafa elsayed <moelsayed949@gmail.com>
2024-02-16 22:45:23 +0200

    styling

--- src/components/AchievementsSection.jsx
+++ src/components/AchievementsSection.jsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { motion } from "framer-motion";
 
 const skills = [
   {
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,37 +1,56 @@
 import React from "react";
 import CodeIcon from "@mui/icons-material/Code";
+import PhoneAndroidIcon from "@mui/icons-material/PhoneAndroid";
+import DesignServicesIcon from "@mui/icons-material/DesignServices";
+import ManageHistoryIcon from "@mui/icons-material/ManageHistory";
+import DeveloperBoardIcon from "@mui/icons-material/DeveloperBoard";
+import DevicesIcon from "@mui/icons-material/Devices";
 const data = [
   {
-    icon: <CodeIcon />,
-    title: "ui&ux",
-    desc: "jdfhdljfhljdfh",
-  },
-  {
-    icon: <CodeIcon />,
+    icon: <DevicesIcon sx={{}}/>,
     title: "Frontend Developing",
     desc: "Making Front End of The Web and Use The Lastest Features OF FrameWorks and Predecessor .",
   },
   {
-    icon: <CodeIcon />,
+    icon: <PhoneAndroidIcon />,
     title: "Mobile App Developer",
-    desc: "jdfhdljfhljdfh",
+    desc: "Designing , Developing Mobile Apps With Cross Platform Framework For Android , IOS .",
   },
   {
-    icon: <CodeIcon />,
-    title: "ui&ux",
-    desc: "jdfhdljfhljdfh",
+    icon: <DesignServicesIcon />,
+    title: "UX/UI Design",
+    desc: "Designing The Web Looking Of Colors, Themes and Fonts With Multi Programms Like Figma , AdobeXD .",
+  },
+  {
+    icon: <ManageHistoryIcon />,
+    title: "SEO Optimize",
+    desc: "Making Sure That The Design Works Perfectly,Loading Speed and Testing For Errors .",
+  },
+  {
+    icon: <DeveloperBoardIcon />,
+    title: "Developing My Skill",
+    desc: "Making Sure That The Design Works Perfectly,Loading Speed and Testing For Errors .",
   },
 ];
 const Skills = () => {
   return (
     <section className="text-black">
-      <div className="flex flex-row gap-4">
+      <div className="text-center pb-6 ">
+        <span className=" text-black dark:text-white p-3 rounded-lg border border-gray-500 font-bold">
+          What I Can Offer
+        </span>
+      </div>
+
+      <div className="flex flex-row gap-2 ">
         {data.map((item) => {
           return (
-            <div key={item.id} className="bg-gray-500 p-8 w-full h-full rounded-lg">
-              <div >{item.icon}</div>
-              <div className="text-white pt-3 pb-3">{item.title}</div>
-              <div>{item.desc}</div>
+            <div
+              key={item.id}
+              className="bg-blue-200 p-8 w-full h-full rounded-lg"
+            >
+              <div className="bg-white rounded-md w-fit p-2">{item.icon}</div>
+              <div className="text-black font-bold pt-3 pb-3">{item.title}</div>
+              <div className="text-black ">{item.desc}</div>
             </div>
           );
         })}
