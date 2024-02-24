b4e1f83242d2b1bf86bdb944254601e6de484439
moustafa elsayed <moelsayed949@gmail.com>
2024-02-24 21:33:50 +0200

    fixed issue

--- src/components/navBar/NavLinks.jsx
+++ src/components/navBar/NavLinks.jsx
@@ -13,7 +13,7 @@ const NavLinks = ({ navLinks, activeSection }) => {
           <li key={index}>
             <Link
               href={item.path}
-              className={`block py-2 pl-3 pr-4 dark:text-white text-black sm:text-xl rounded md:p-0 hover:text-blue-400 dark:hover:text-blue-400 font-semibold ${
+              className={`block py-2 pl-3 pr-4 dark:text-white text-black sm:text-xl rounded md:p-0 hover:text-blue-400 dark:hover:text-yellow-400 font-semibold ${
                 item.path === `#${activeSection}` ? "text-blue-400 dark:text-yellow-400 " : ""
               }`}
             >
