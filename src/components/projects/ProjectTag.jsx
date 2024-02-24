947116828c382f755454a4a0ef046286949b5520
moustafa elsayed <moelsayed949@gmail.com>
2024-02-24 22:07:01 +0200

    add items

--- src/components/projects/ProjectTag.jsx
+++ src/components/projects/ProjectTag.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const ProjectTag = ({ name, onClick, isSelected }) => {
   const buttonStyles = isSelected
-    ? "text-blue-500 border-blue-500"
-    : "text-[#ADB7BE] border-slate-600 hover:border-blue-500";
+    ? "text-blue-500 border-blue-500 dark:text-yellow-500 dark:border-yellow-500"
+    : "text-[#ADB7BE] border-slate-600 hover:border-blue-500 dark:hover:border-yellow-500";
   return (
     <button
       className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}
