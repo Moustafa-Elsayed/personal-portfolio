62566629d186f4a49e1cfd9309d7247a5074111c
moustafa elsayed <moelsayed949@gmail.com>
2024-02-28 11:55:40 +0200

    fixed issue

--- src/components/aboutme/TabButton.jsx
+++ src/components/aboutme/TabButton.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const TabButton = ({ active, selectTab, children }) => {
   const buttonClasses = active
-    ? "text-blue-500 border-b border-blue-500"
+    ? "text-blue-500 border-b border-blue-500 dark:text-yellow-400 dark:border-yellow-400"
     : "text-[#ADB7BE]";
 
   return (
     <button onClick={selectTab}>
-      <p className={`mr-3 font-semibold  hover:text-white ${buttonClasses}`}>
+      <p className={`mr-3 font-semibold  hover:text-blue-500 dark:hover:text-yellow-400  ${buttonClasses}`}>
         {children}
       </p>
     </button>
